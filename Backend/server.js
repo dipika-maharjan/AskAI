ade85a7103e36d62be2302c511d62426b1adc9e9
Dipika <maharjandipika49@gmail.com>
2026-05-01 18:01:06 +0545

    backend server added

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,38 @@
-import { GoogleGenAI } from "@google/genai";
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
 
-const ai = new GoogleGenAI({
-  apiKey: process.env.GEMINI_API_KEY,
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`server running at ${PORT}`);
 });
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-1.5-flash",
-    contents: "Explain how AI works in a few words",
-  });
-
-  console.log(response.text);
-}
-
-main();
+app.post("/test", async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "",
+            messages: [{
+                role: "user",
+                content: req.body.message
+            }]
+        })
+    };
+    try{
+        const response = await fetch("https://openrouter.ai/api/v1/chat/completions", options);
+        const data = await response.json();
+        console.log(data.choices[0].message.content);
+        res.send(data.choices[0].message.content);
+    }catch(err){
+        console.log(err);
+    }
+});
