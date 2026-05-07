7c5777c365c48d7349f19331bb06f70f98e0c53b
Dipika <maharjandipika49@gmail.com>
2026-05-07 11:15:27 +0545

    chat thread updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,10 +9,15 @@ function App() {
   const [prompt, setPrompt] = useState("");
   const [reply, setReply] = useState(null);
   const [currThreadId, setCurrThreadId] = useState(uuidv1());
+  const [prevChats, setPrevChats] = useState([]); //stores all chats of current thread
+  const [newChat, setNewChat] = useState(true); //track if new chat is being created
+
   const providerValues = {
     prompt, setPrompt,
     reply, setReply,
-    currThreadId, setCurrThreadId
+    currThreadId, setCurrThreadId,
+    newChat, setNewChat,
+    prevChats, setPrevChats
   };  //passing values
 
   return (
